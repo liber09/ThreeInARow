0be5bcd8feb308af8163f792089ded3527d83fdf
liber09 <113246195+liber09@users.noreply.github.com>
2022-09-27 23:18:07 +0200

    Tried different board printouts

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("**\n-:Ö.- . **");
-        System.out.println("| |");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        gamingBoard();
+
+    }
+    public static void gamingBoard(){
+        String[] position = new String[8];
+        position[0] = " ";
+        position[1] = "X";
+        position[2] = "0";
+        //method for writing out the start gamingbord.
+        System.out.println("---+---+---+");
+        System.out.println(" "+position[0]+" | "+position[1]+" | "+ position[2]+" ");
+        System.out.println("---+---+---+");
+        System.out.println(" O |   | X ");
+        System.out.println("---+---+---+");
+        System.out.println(" X | X | O ");
+        System.out.println("---+---+---+");
     }
 }
